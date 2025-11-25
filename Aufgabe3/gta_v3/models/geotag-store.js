3f1496940c1cbde86d913272d1f2566217d12943
Pius-06 <tebbe.pius7@gmail.com>
2025-11-25 17:31:18 +0100

    implement these methods: addGeoTag, removeGeoTag, getNearbyGeoTags, searchNearbyGeoTags

--- Aufgabe3/gta_v3/models/geotag-store.js
+++ Aufgabe3/gta_v3/models/geotag-store.js
@@ -23,10 +23,49 @@
  * - The proximity constrained is the same as for 'getNearbyGeoTags'.
  * - Keyword matching should include partial matches from name or hashtag fields. 
  */
-class InMemoryGeoTagStore{
+class InMemoryGeoTagStore {
 
     // TODO: ... your code here ...
+    #tags = [];
+    addGeoTag(tag) {
+        this.#tags.push(tag);
+    }
 
+    removeGeoTag(tagName) {
+        this.#tags = this.#tags.filter(tag => tag.name !== tagName);
+    }
+
+    // Latitude (Breitengrad): Gibt an, wie weit nördlich oder südlich man vom Äquator ist.
+    // Longitude (Längengrad): Gibt an, wie weit östlich oder westlich man vom Nullmeridian ist.
+    getNearbyGeoTags(latitude, longitude, distance) {
+        let nearbyTags = [];
+        this.#tags.forEach(tag => {
+            if (this.isTagNearbyLocation(latitude, longitude, distance, tag)) {
+                nearbyTags.push(tag);
+            }
+        });
+        return nearbyTags;
+    }
+
+    searchNearbyGeoTags(latitude, longitude, distance, searchTerm) {
+        let nearbyTags = this.getNearbyGeoTags(latitude, longitude, distance);
+        let nearbyTagsWithTerm = [];
+        nearbyTags.forEach(nearbyTag => {
+            if (nearbyTag.name.includes(searchTerm) || tag.hashtag.includes(searchTerm)) {
+                nearbyTagsWithTerm.push(nearbyTag);
+            }
+        });
+        return nearbyTagsWithTerm;
+    }
+
+    isTagNearbyLocation(latitude, longitude, distance, tag) {
+        if ((tag.latitude < latitude + distance) && (tag.latitude > latitude - distance)) {
+            if ((tag.longitude < longitude + distance) && (tag.longitude > longitude - distance)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
 module.exports = InMemoryGeoTagStore
